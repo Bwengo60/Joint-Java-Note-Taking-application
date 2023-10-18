6306a186e97c0e9c215407fd833792befa520440
Bwengo60 <58523713+Bwengo60@users.noreply.github.com>
2023-10-18 20:20:56 +0200

    Update to last
    Just added more functions and showing everyones roles in the comments

--- NoteTakingApp.java
+++ NoteTakingApp.java
@@ -13,6 +13,7 @@ class NoteTakingApp{
              switch(choice){
                  case "1":
                        System.out.println("Logged in!");
+                       home();
                        break;
                  case "2":
                        System.out.println("Registered");//Replace me with a working registration function.
@@ -27,15 +28,53 @@ class NoteTakingApp{
              
          }
     }
-    void login(){
+    void login(){ // Monde your task
+
        //Please enter the login code here!
       // And please make sure the code code is running in a loop so that if can tell the user that they have entered wrong information and give them provision to correct their mistake.
 
       // Monde your work is what to do after the user logs in,, you have to check if the user is an admin or a normal user
     }
-   void register(){
+   void register(){// Clevis this is your task
       //Please enter the register code here!
       // And please make sure the code code is running in a loop so that if can tell the user that they have entered wrong information and give them provision to correct their mistake.
+
+      // Remember we are putting the users in a temporary database made using a collection
+   }
+  static void home(){// This was my task "Muhammadi Bwengo" 
+      boolean homeLoop = true;
+      Scanner scanner = new Scanner(System.in);
+      
+      while(homeLoop){
+          System.out.println("Welcome to the Home page\n\n1. Create New Notes\n2. Read Notes\n3. Update Notes\n4. Delete Notes\n5. Logout");
+      String homeChoice = scanner.nextLine();
+      switch(homeChoice){
+          case "1":
+              //Add the create notes function here
+              System.out.println("Notes Created!");
+              break;
+          case "2":
+              //Add the read notes function here
+              System.out.println("Read Notes");
+              break;
+          case "3":
+              //Add the Update Notes function here
+              System.out.println("");
+              break;
+          case "4":
+              // Add the delete notes function here
+              System.out.print("Delete notes");
+              break;
+          case "5":
+              System.out.println("You logged out!");
+              homeLoop = false;
+       }
+      }
+      
+   }
+
+   static void createNotes(){// Webster your task is here 
+        // Fill this function with working code to create notes remember we are storing the data in a collection
    }
 }
 
